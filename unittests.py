96667a295685522a80322cead726b4a117de82d6
ben-price13 <benjamin.price@colorado.edu>
2020-12-17 12:14:14 -0700

    Added test to ensure number of entries is correct

--- unittests.py
+++ unittests.py
@@ -19,6 +19,10 @@ class TestConvertJSONToSQL(unittest.TestCase):
         correct_description = "Aid grants the target a +1 morale bonus on attack rolls and saves against fear effects, plus temporary hit points equal to 1d8 + caster level (to a maximum of 1d8+10 temporary hit points at caster level 10th)."
         self.assertEqual(correct_description, self.c.fetchone()[0])
 
+    def test_num_spells(self):
+        self.c.execute("SELECT name FROM spells")
+        self.assertEqual(4, len(self.c.fetchall()) )
+
     def tearDown(self):
         print("---> deleting test.db...\n")
         self.conn.close()
