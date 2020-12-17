f3ede9a2e36a198499578b1c40fae953c9fd2dd4
ben-price13 <benjamin.price@colorado.edu>
2020-12-17 12:02:24 -0700

    updated setUP and tearDown print messages to be clearer on command line

--- unittests.py
+++ unittests.py
@@ -5,17 +5,20 @@ import os
 
 class TestConvertJSONToSQL(unittest.TestCase):
     def setUp(self):
+        print("\n---> setting up test.db...\n")
         os.system('py convertJSONtoSQL.py testData.json test.db')
         self.conn = sqlite3.connect('test.db')
-        self.c = conn.cursor()
+        self.c = self.conn.cursor()
 
     def test_spells_table_exists(self):
         self.c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='spells'");
         self.assertEqual(1, len(self.c.fetchall()))
 
     def tearDown(self):
-        os.system(del test.db)
+        print("---> deleting test.db...\n")
         self.conn.close()
+        os.system('del test.db')
+
 
 if __name__ == '__main__':
     unittest.main()
