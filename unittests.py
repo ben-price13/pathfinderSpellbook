05ad837a9414f3b5105bcb95b4aa7e64a4755648
ben-price13 <benjamin.price@colorado.edu>
2020-12-17 12:07:03 -0700

    Added test to check for spell name being in table correctly

--- unittests.py
+++ unittests.py
@@ -14,6 +14,11 @@ class TestConvertJSONToSQL(unittest.TestCase):
         self.c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='spells'");
         self.assertEqual(1, len(self.c.fetchall()))
 
+    def test_spell_names(self):
+        self.c.execute("SELECT description FROM spells WHERE name='Aid'")
+        correct_description = "Aid grants the target a +1 morale bonus on attack rolls and saves against fear effects, plus temporary hit points equal to 1d8 + caster level (to a maximum of 1d8+10 temporary hit points at caster level 10th)."
+        self.assertEqual(correct_description, self.c.fetchone()[0])
+
     def tearDown(self):
         print("---> deleting test.db...\n")
         self.conn.close()
